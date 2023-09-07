9f4357eda9525a19d5d044ecdb944a3e676a3972
chidicolly11@gmail.com <chidicolly11@gmail.com>
2023-09-07 15:54:26 +0100

    magneto shoping

--- src/pages/signin.jsx
+++ src/pages/signin.jsx
@@ -17,8 +17,10 @@ export default function Signin() {
   const OnSubmit = (e) => {
     e.preventDefault();
     try {
-      axios.post ('https://shopping-today.onrender.com/api/accounts/login', inputs).then((res) => {
-        console.log(res.data);
+      axios.post ('https://magneto-bqfl.onrender.com/api/accounts/login').then((res) => {
+        setInputs(res.data.inputs)
+        console.log(res.data); 
+
         navigate('/')
         alert('Login successfully');
       })
